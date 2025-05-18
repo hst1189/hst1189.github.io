59f0e28e18afac16ba86ccd858e81f51fb0d7b86
hst1189 <hst1189@hotmail.com>
2025-05-18 16:19:18 +0900

    Update musicPlayer.js

--- static/assets/musicPlayer.js
+++ static/assets/musicPlayer.js
@@ -1,9 +1,9 @@
 const music_div = document.createElement('div')
-music_div.id = 'player'   // 设置元素id
+music_div.id = 'music_player'   // 设置元素id
 document.body.appendChild(music_div)  // 插入到body元素最后
 
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('music_player'),
     listFolded: false,
     listMaxHeight: 90,
     lrcType: 3,
@@ -11,6 +11,13 @@ const ap = new APlayer({
         name: 'Time after time ～花舞う街で～',
         artist: '倉木 麻衣',
         url: '../music/TimeAfterTime.mp3',
-        cover: 'images/other/11056086_074392.jpg',
+        lrc: '../music/TimeAfterTime.lrc',
+        cover: '../music/cover1.jpg',
+        theme: '#ebd0c2'
     }]
+
+
+
+
+    
 });
