cdc7ed87482a2bf1ef4d679e2e694defbbf8d461
hst1189 <hst1189@hotmail.com>
2025-05-18 15:33:02 +0900

    Add files via upload

---
+++ static/assets/musicPlayer.js
@@ -0,0 +1,16 @@
+const music_div = document.createElement('div')
+music_div.id = 'player'   // 设置元素id
+document.body.appendChild(music_div)  // 插入到body元素最后
+
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    listFolded: false,
+    listMaxHeight: 90,
+    lrcType: 3,
+    audio: [{
+        name: 'Time after time ～花舞う街で～',
+        artist: '倉木 麻衣',
+        url: '../music/TimeAfterTime.mp3',
+        cover: 'images/other/11056086_074392.jpg',
+    }]
+});
